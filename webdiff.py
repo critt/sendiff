9e01cd60a0fa06383a8fe248d50f8cf60123f337
chris sims <cbarsim@gmail.com>
2018-08-17 15:25:55 -0700

    Went back to JSON config file from python config file. Implemented Userconfig and Target classes.

--- webdiff.py
+++ webdiff.py
@@ -1,57 +1,72 @@
-from time import gmtime, strftime, sleep
 from bs4 import BeautifulSoup
 import requests
 import time
-import json
-import json
 import yagmail
-
-email = None
-pw = None
-recipient = None
-target_url = None
-target_label = None
-
-with open('config.json') as json_file:  
-    data = json.load(json_file)
-
-    email = data['email']
-    pw = data['pw']
-    recipient = data['recipient']
-    target_url = data['target_url']
-    target_label = data['target_label']
+import json
 
 
-compare_to = None
+class Target:
+    def __init__(self, recipient, target_url, target_label):
+        self.recipient = recipient
+        self.target_url = target_url
+        self.target_label = target_label
 
-n = 0
-while 1 > 0:
+
+class Userconfig:
+    def __init__(self):
+        with open('config.json', 'r') as f:
+            cfg = json.load(f)
+
+            self.sender_username = cfg['sender_username']
+            self.sender_pw = cfg['sender_pw']
+            self.targets = []
+            for target in cfg['targets']:
+                self.targets.append(Target(**target))
+
+
+def send(recipient, subject, body, sender, pw):
+    try:
+        yag = yagmail.SMTP(sender, pw)
+        yag.send(to=recipient, subject=subject, contents=body)
+        print('Email sent!')
+    except Exception as e:
+        print('email error: %s' % e)
+        e.print_exc()
+
+
+def fullTextDiffThread(sender, pw, recipient, target_url, target_label):
+    print(recipient)
+    before = None
+    n = 0
+    while 1 > 0:
         print(n)
         r = requests.get(target_url)
         soup = BeautifulSoup(r.text, 'html.parser')
-        tmp = (soup.getText())
-        #print tmp
-        if compare_to != tmp and n != 0:
-            #send email
-            gmail_user = email 
-            gmail_password = pw
+        after = (soup.getText())
 
-            sent_from = gmail_user  
-            to = [recipient]  
+        if n != 0 and before != after:
+
             subject = 'diff found in %s' % target_label
             body = target_url
 
-            try:  
-                yag = yagmail.SMTP(email, pw)
-                yag.send(to = recipient, subject = subject, contents = body)
-                print('Email sent!')
-            except Exception as e:  
-                print('email error: %s' % e)
-                traceback.print_exc()
+            send(recipient, subject, body, sender, pw)
 
         else:
-            compare_to = tmp
+            before = after
             print('no diff')
-        #sleep in seconds
+
+        # sleep in seconds
         n = 1
         time.sleep(20)
+
+
+def main(u_config):
+    for target in u_config.targets:
+        m_recipient = target.recipient
+        m_target_url = target.target_url
+        m_target_label = target.target_label
+        fullTextDiffThread(u_config.sender_username, u_config.sender_pw, m_recipient, m_target_url, m_target_label) # noqa E501
+
+
+u_config = Userconfig()
+main(u_config)

3f6e5531f5e0293d7f61f935966d07dd45b99965
chris sims <cbarsim@gmail.com>
2018-08-18 22:10:30 -0700

    implemented email queue

--- webdiff.py
+++ webdiff.py
@@ -10,14 +10,6 @@ from queue import Queue
 print_lock = threading.Lock()
 
 
-class Target:
-    def __init__(self, recipient, target_url, target_label, interval):
-        self.recipient = recipient
-        self.target_url = target_url
-        self.target_label = target_label
-        self.interval = interval
-
-
 class Userconfig:
     def __init__(self):
         with open('config.json', 'r') as f:
@@ -30,29 +22,51 @@ class Userconfig:
                 self.targets.append(Target(**target))
 
 
-def send(recipient, subject, body, sender, pw):
+class Target:
+    def __init__(self, recipient, target_url, target_label, interval):
+        self.recipient = recipient
+        self.target_url = target_url
+        self.target_label = target_label
+        self.interval = interval
+
+
+class Email:
+    def __init__(self, recipient, subject, body):
+        self.recipient = recipient
+        self.subject = subject
+        self.body = body
+
+
+def send(email):
+    with print_lock:
+        print('%s sending' % email.subject)
     try:
-        yag = yagmail.SMTP(sender, pw)
-        yag.send(to=recipient, subject=subject, contents=body)
+        yag = yagmail.SMTP(u_config.sender_username, u_config.sender_pw)
+        yag.send(to=email.recipient, subject=email.subject, contents=email.body) # noqa E501
         with print_lock:
-            print('%s Email sent!' % subject)
+            print('%s Email sent!' % email.subject)
     except Exception as e:
         with print_lock:
             print('email error: %s' % e)
 
 
-def fullTextDiff(sender, pw, recipient, target_url, target_label, interval):
+def fullTextDiff(recipient, target_url, target_label, interval):
     before = None
     should_diff = False
-    while True:
+    for n in range(0, 3):
         r = requests.get(target_url)
         soup = BeautifulSoup(r.text, 'html.parser')
         after = (soup.getText())
 
         if should_diff and before != after:
+
+            with print_lock:
+                print('%s diff found' % target_label)
+
             subject = 'diff found in %s' % target_label
             body = target_url
-            send(recipient, subject, body, sender, pw)
+            # send(Email(recipient, subject, body))
+            send_queue.put(Email(recipient, subject, body))
 
         else:
             before = after
@@ -63,21 +77,35 @@ def fullTextDiff(sender, pw, recipient, target_url, target_label, interval):
         time.sleep(math.ceil(float(interval) * 60))
 
 
-def thread_queue():
+def request_queue():
     while True:
-        current_target = target_queue.get()
-        fullTextDiff(u_config.sender_username, u_config.sender_pw, current_target.recipient, current_target.target_url, current_target.target_label, current_target.interval) # noqa E501
-        target_queue.task_done()
+        current_target = scan_queue.get()
+        fullTextDiff(current_target.recipient, current_target.target_url, current_target.target_label, current_target.interval) # noqa E501
+        scan_queue.task_done()
+
+
+def email_queue():
+    while True:
+        current_email = send_queue.get()
+        send(current_email)
+        send_queue.task_done()
 
 
 u_config = Userconfig()
 
-target_queue = Queue()
+scan_queue = Queue()
+send_queue = Queue()
 
 for target in u_config.targets:
-    t = threading.Thread(target=thread_queue)
+    t = threading.Thread(target=request_queue)
     t.daemon = True
     t.start()
-    target_queue.put(target)
+    scan_queue.put(target)
 
-target_queue.join()
+
+t = threading.Thread(target=email_queue)
+t.daemon = True
+t.start()
+
+scan_queue.join()
+send_queue.join()
